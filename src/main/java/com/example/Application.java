eed554cb3aaba6a5b7a96a23e5f28bbef4c446d6
Lucas <lucasvalenteds@gmail.com>
2022-03-15 12:16:19 -0300

    Configuring JDBC Repositories and bean scan

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -2,12 +2,15 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 
 @SpringBootApplication
+@ComponentScan("com.example")
+@EnableJdbcRepositories
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
 }
